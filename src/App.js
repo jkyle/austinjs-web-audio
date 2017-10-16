5d5542902203bec268aed39b7d2e1b6b0b6c0e3b
jkyle <jkyle@causethought.com>
2017-10-16 16:24:57 -0500

    Added multi-oscillator

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import style from './style.styl'
 import Device from './Device'
 import TimeAnalyser, { TimeAnalyserDOM } from './TimeAnalyser'
 import FrequencyAnalyser, { FrequencyAnalyserDOM } from './FrequencyAnalyser'
-import Oscillator, { OscillatorDOM } from './Oscillator'
+import MultiOsc, { MultiOscDOM } from './Multiosc'
 
 const context = new (window.AudioContext || window.webkitAudioContext)()
 const timeAnalyser = TimeAnalyser(context)
 const freqAnalyser = FrequencyAnalyser(context)
-const osc = Oscillator(context)
+const osc = MultiOsc(context)
 const gain = context.createGain()
 osc.gain.connect(gain)
 gain.connect(context.destination)
@@ -24,7 +24,7 @@ setInterval(() => play(context.currentTime + 0.5), 2000)
 export default () => (
   <div className={style.container}>
     <Device name="Oscillator">
-      <OscillatorDOM device={osc} />
+      <MultiOscDOM device={osc} />
     </Device>
     <Device name="Time Analyser">
       <TimeAnalyserDOM device={timeAnalyser} />
