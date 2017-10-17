1b78e48b260a207193c51757a360427edffb166e
jkyle <jkyle@causethought.com>
2017-10-16 20:19:54 -0500

    Add Clock and Sequencer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MultiOsc, { MultiOscDOM } from './Multiosc'
 import Keyboard, { KeyboardDOM } from './Keyboard'
 import Filter, { FilterDOM } from './Filter'
 import LFO, { LFODOM } from './LFO'
+import Clock, { ClockDOM } from './Clock'
+import Sequencer, { SequencerDOM } from './Sequencer'
 
 const context = new (window.AudioContext || window.webkitAudioContext)()
 const keyboard = Keyboard(context)
@@ -26,10 +28,23 @@ gain.connect(context.destination)
 gain.connect(timeAnalyser.analyser)
 gain.connect(freqAnalyser.analyser)
 
+const clock = Clock(context)
+const sequencer = Sequencer(context)
+
+clock.register(sequencer.onTempoChange)
+clock.quarterEvents.listen(sequencer.trigger)
+sequencer.noteOn.listen(osc.start)
+
 export default () => (
   <div className={style.container}>
     <Device name="Keyboard">
       <KeyboardDOM device={keyboard} />
+    </Device>
+    <Device name="Clock">
+      <ClockDOM device={clock} />
+    </Device>
+    <Device name="Sequencer">
+      <SequencerDOM device={sequencer} />
     </Device>
     <Device name="Oscillator">
       <MultiOscDOM device={osc} />
