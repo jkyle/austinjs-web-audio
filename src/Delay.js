116f7e487c4625c5053a698cf7c751aa35bf2e0a
jkyle <jkyle@causethought.com>
2017-10-16 20:42:41 -0500

    Prep for Delay

--- src/Delay.js
+++ src/Delay.js
@@ -2,10 +2,10 @@ import React from 'react'
 import style from './style.styl'
 import makeDevice from './Generic'
 import Knob from './Knob'
+import eventBus from './event-bus'
 
 const Delay = (context, time = 0, feedbackAmout = 0) => {
-  const listeners = []
-  const register = listener => listeners.push(listener)
+  const events = eventBus()
 
   const delay = context.createDelay()
   delay.delayTime.value = time
@@ -13,38 +13,52 @@ const Delay = (context, time = 0, feedbackAmout = 0) => {
   const feedback = context.createGain()
   feedback.gain.value = feedbackAmout
 
+  const dry = context.createGain()
+  const input = context.createGain()
+  const wet = context.createGain()
   const out = context.createGain()
-  out.gain.value = 0
+
+  dry.gain.value = 1
+  wet.gain.value = 0
+
+  dry.connect(out)
+  wet.connect(out)
 
   delay.connect(feedback)
   feedback.connect(delay)
-  delay.connect(out)
+
+  delay.connect(wet)
+  input.connect(dry)
+  input.connect(delay)
 
   const onValueChange = (target, value) => {
     target.value = value
-    listeners.forEach(listener => listener())
+    events.trigger(value)
   }
 
   const onDelayTimeChange = (value) => {
     delay.delayTime.value = value
-    listeners.forEach(listener => listener())
+    events.trigger(value)
   }
 
   const onFeedbackChange = (value) => {
     feedback.gain.value = value
-    listeners.forEach(listener => listener())
+    events.trigger(value)
   }
 
   const onGainChange = (value) => {
-    out.gain.value = value
-    listeners.forEach(listener => listener())
+    dry.gain.value = 1 - value
+    wet.gain.value = value
+    events.trigger(value)
   }
 
   return {
-    register,
+    register: events.listen,
     delay,
     feedback,
     out,
+    wet,
+    input,
     onDelayTimeChange,
     onFeedbackChange,
     onGainChange,
@@ -57,21 +71,21 @@ const DelayDOM = makeDevice(({ device }) => (
     <Knob label="mix"
       min={0}
       max={1}
-      step={0.2}
-      value={device.out.gain.value}
-      onChange={value => device.onValueChange(device.out.gain, value)}
+      step={0.02}
+      value={device.wet.gain.value}
+      onChange={value => device.onGainChange(value)}
     />
     <Knob label="time"
       min={0}
       max={1}
-      step={0.2}
+      step={0.02}
       value={device.delay.delayTime.value}
       onChange={value => device.onValueChange(device.delay.delayTime, value)}
     />
     <Knob label="fdbk"
       min={0}
       max={0.9}
-      step={0.2}
+      step={0.02}
       value={device.feedback.gain.value}
       onChange={value => device.onValueChange(device.feedback.gain, value)}
     />
